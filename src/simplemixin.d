48dc81bf5f5890e6341d952d44a3e6fe9b78cb35
adilbaig <adil.baig@aidezigns.com>
2014-07-13 16:08:58 +0530

    simple mixin

--- src/simplemixin.d
+++ src/simplemixin.d
@@ -1,8 +1,13 @@
 import std.stdio;
+import std.string : format;
+import std.range : iota;
 
 void main()
 {
 	writeln("Hello World");
+	
+	foreach(i; iota(50))
+		writeln(format("*%d\r\n%s", 32, "What's up"));
 	
 	pragma(msg, "writeln(\"Hello World\");");
 	mixin("writeln(\"Hello World\");");
